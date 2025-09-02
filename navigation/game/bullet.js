5934333e6bc6d0a34230ed65b91f4cc21bb8af95
Arnav210 <arnavmittal210@gmail.com>
2025-09-02 10:48:04 -0700

    updated OOP for bullet

--- navigation/game/bullet.js
+++ navigation/game/bullet.js
@@ -1,61 +1,55 @@
-
-import { player, pointAt, move } from './move.js';
+import { player, pointAt } from './move.js';
 import { camera } from './camera.js';
 import { checkOnscreen } from './screen.js';
 
-export const bullet = {
-    x: 0,
-    y: 0,
-    xv: 0,
-    yv: 0,
-    speed: 10,
-    dir: 0,
-    life: 100,
-};
+export class Bullet {
+    static bullets = []; // holds all active bullets
 
-export var bullets = [];
+    constructor(x, y, type = 0) {
+        pointAt(x, y);
+        const angle = player.dir * (Math.PI / 180);
 
-export function shootBullet(x,y,type=0) {
-    pointAt(x, y);
-    const angle = player.dir * (Math.PI / 180);
-    if (type === 1) {
-        bullet.life = 100;
-    } else {
-        bullet.life = 50;
+        this.x = player.x + Math.cos(angle) * 20;
+        this.y = player.y + Math.sin(angle) * 20;
+        this.xv = Math.cos(angle) * Bullet.speed;
+        this.yv = Math.sin(angle) * Bullet.speed;
+        this.speed = Bullet.speed;
+        this.dir = player.dir;
+        this.life = (type === 1) ? 100 : 50;
+
+        Bullet.bullets.push(this);
+
+        player.ammo -= 1;
+        if (player.ammo <= 0) player.ammo = 3;
     }
-    const newBullet = {
-        x: player.x + Math.cos(angle) * 20,
-        y: player.y + Math.sin(angle) * 20,
-        xv: Math.cos(angle) * bullet.speed,
-        yv: Math.sin(angle) * bullet.speed,
-        speed: bullet.speed,
-        dir: player.dir,
-        life: bullet.life,
-    };
-    bullets.push(newBullet);
-    player.ammo -= 1;
-    if (player.ammo <= 0) player.ammo = 3;
-};
 
-export function updBullets(ctx, canvas) {
-    for (let i = 0; i < bullets.length; i++) {
-        const b = bullets[i];
-        b.x += b.xv;
-        b.y += b.yv;
-        b.life -= 1;
-        if (b.life <= 0) {
-            bullets.splice(i, 1);
-            i--;
-            continue;
+    // properties shared by all bullets
+    static speed = 10;
+
+    // update and render all bullets
+    static updateAll(ctx, canvas) {
+        for (let i = 0; i < Bullet.bullets.length; i++) {
+            const b = Bullet.bullets[i];
+            b.update(ctx, canvas, i);
         }
-        if (!checkOnscreen(b.x, b.y, canvas.width, canvas.height)) {
-            bullets.splice(i, 1);
-            i--;
-            continue;
+    }
+
+    // update this bullet
+    update(ctx, canvas, index) {
+        this.x += this.xv;
+        this.y += this.yv;
+        this.life -= 1;
+
+        // remove if expired or off-screen
+        if (this.life <= 0 || !checkOnscreen(this.x, this.y, canvas.width, canvas.height)) {
+            Bullet.bullets.splice(index, 1);
+            return;
         }
+
+        // draw
         ctx.fillStyle = 'black';
         ctx.beginPath();
-        ctx.arc((b.x - camera.x) + canvas.width/2, (b.y - camera.y) + canvas.height/2, 5, 0, Math.PI * 2);
+        ctx.arc((this.x - camera.x) + canvas.width / 2, (this.y - camera.y) + canvas.height / 2, 5, 0, Math.PI * 2);
         ctx.fill();
     }
-};
+}
